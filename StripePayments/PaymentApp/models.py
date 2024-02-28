a5a6ae3a22a07eff2a07265499ffd7737a83f27f
k-mickey <kokmickey@yandex.ru>
2024-02-28 15:37:34 +0300

    add Item model

--- StripePayments/PaymentApp/models.py
+++ StripePayments/PaymentApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование',
+        max_length=255,
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+        default=0,
+    )
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
